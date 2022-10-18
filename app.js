4aced8d6b8cda0bc3cbfde75e5c4872788d79724
abenezerGithub <95424874+abenezerGithub@users.noreply.github.com>
2022-10-18 15:20:20 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ let server = require("http").createServer((req,res) => {
 })
 let fs = require("fs")
 
-fs.writeFile("/abux.txt","text",() =} console.log("text created"))
+fs.writeFile("/abux.txt","text",() => console.log("text created"))
 server.listen(process.env.PORT || 5000) 
